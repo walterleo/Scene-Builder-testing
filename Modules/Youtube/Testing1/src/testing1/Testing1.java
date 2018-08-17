327d2c632927871e35c1c4d7d0d9f0d625fb4ed3
Walter <Walter@DESKTOP-82TFBEC.NGV.local>
2018-08-17 11:39:58 +1000

    Scene Builder testingnew

---
+++ Modules/Youtube/Testing1/src/testing1/Testing1.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testing1;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Walter
+ */
+public class Testing1 extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        //primaryStage.setTitle("Hello");
+        //primaryStage.setScene(new Scene(root, 800, 500));
+        //primaryStage.show();
+        
+        Scene scene;
+        scene = new Scene(root, 800, 500);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
